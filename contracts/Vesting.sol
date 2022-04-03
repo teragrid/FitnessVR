62c5c491c96c56a31a173efb41e6c122b1147df8
tantv4668 <tantv4668@gmail.com>
2022-04-03 10:15:17 +0700

    add vesting contract

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -8,159 +8,253 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Vesting is Ownable {
     using SafeERC20 for IERC20;
 
-    uint32 private constant SECONDS_PER_DAY = 24 * 60 * 60;
+    enum VestingType {
+        MONTHLY,
+        NEARLY
+    }
 
-    IERC20 private _token;
-    bool private _isSetSchedule;
-    uint256 private _totalAmountInvested;
-    Schedule private _schedule;
-    uint8 private _tgeUnlockPercent;
-    uint32 private _tgeUnlockDate;
-
-    struct User {
+    struct UserVestingInfo {
         uint256 amount;
+        uint256 tgeUnlockPercentage;
         uint256 amountClaimed;
-        uint256 tgeAmountClaimed;
+        uint256 cliffPeriod; // calculated by the number of blocks
+        uint256 vestingDuration; // calculated by the number of unlock period
+        VestingType vestingType;
+        bool exist;
     }
 
-    struct Schedule {
-        uint32 startDate;
-        uint32 cliffPeriodDate;
-        uint32 interval;
-        uint32 milestones;
-    }
+    uint256 public unlockPeriod; // calculated by the number of blocks
 
-    mapping(address => User) private _users;
+    uint32 public constant SECONDS_PER_BLOCK = 3;
+
+    IERC20 public token;
+
+    uint256 public tgeTimestamp;
+
+    uint256 public totalVesting;
+
+    mapping(address => UserVestingInfo) public userToVesting;
 
     event AddUser(address indexed account, uint256 indexed amount);
-    event AddManyUser(address[] indexed accounts, uint256[] amounts);
     event RemoveUser(address indexed account);
-    event SetTGE(uint8 percent, uint32 tgeUnlockDate);
-    event SetVestingSchedule(
-        uint32 startDate,
-        uint32 cliffPeriodDate,
-        uint32 interval,
-        uint32 milestones
-    );
-    event WithdrawTGEUnlock(address indexed user, uint256 amount);
-    event WithdrawToken(address indexed user, uint256 amount);
+    event Claim(address indexed account, uint256 indexed amount);
 
-    constructor(address token) {
-        require(token != address(0), "Vesting/constructor: token address must not be 0");
-        _token = IERC20(token);
-        _isSetSchedule = false;
-    }
-
-    modifier isUserInVesting(address _addr) {
-        require(_users[_addr].amount > 0, "Vesting/modifier: user is not in vesting");
-        _;
-    }
-
-    modifier isVestingScheduled() {
-        require(_isSetSchedule, "Vesting/setVestingSchedule: vesting need to be scheduled!");
-        _;
-    }
-
-    function setTGEUnlock(uint8 _percent, uint32 _tgeDate) public onlyOwner isVestingScheduled(){
-        require(_percent > 0, "Vesting/setTGEUnlock: TGE unlock percent must greater than 0");
-        require(_tgeDate < _schedule.cliffPeriodDate, "Vesting/setTGEUnlock: TGE unlock date must before cliff period date");
-        _tgeUnlockPercent = _percent;
-        _tgeUnlockDate = _tgeDate;
-        emit SetTGE(_percent, _tgeDate);
-    }
-
-    function setVestingSchedule(
-        uint32 _startDate,
-        uint32 _cliffPeriod,
-        uint32 _interval,
-        uint32 _milestones
-    ) public onlyOwner {
-        require(!_isSetSchedule, "Vesting/setVestingSchedule: vesting was already scheduled!");
-        require(block.timestamp / SECONDS_PER_DAY < _startDate, "Vesting/setVestingSchedule: schedule start date after current date");
-        require(_cliffPeriod % _interval == 0, "Vesting/setVestingSchedule: cliff period must divisible by interval");
-
-        _schedule = Schedule(_startDate, _startDate + _cliffPeriod, _interval, _milestones);
-        _isSetSchedule = true;
-        emit SetVestingSchedule(_startDate, _startDate + _cliffPeriod, _interval, _milestones);
-    }
-
-    function addOneUser(address account, uint256 amount)
-        public
-        onlyOwner
-        isVestingScheduled()
-    {
-        require(amount > 0, "Vesting/addOneUser: insufficient amount");
-        require(block.timestamp / SECONDS_PER_DAY < _schedule.startDate, "Vesting/addOneUser: can not add subscriber after vesting started");
-        require(_users[account].amount == 0, "Vesting/addOneUser: user is already in vesting");
-        require(_token.balanceOf(address(this)) >= _totalAmountInvested + amount, "Vesting/addOneUser: not enough supply in pool");
-
-        _totalAmountInvested += amount;
-        _users[account] = User(amount, 0, 0);
-        emit AddUser(account, amount);
-    }
-
-    function addManyUser(address[] memory accounts, uint256[] memory amounts)
-        public
-        onlyOwner
-    {
-        //require acc, amount length > 0
+    constructor(
+        address _token,
+        uint256 _tgeTimestamp,
+        uint256 _unlockPeriod
+    ) {
         require(
-            accounts.length > 0 && amounts.length > 0,
-            "Vesting/addManyUser: accounts and amounts list can not be null"
+            _token != address(0),
+            "Vesting/constructor: Token address must not be zero address"
         );
-        require(accounts.length == amounts.length, "Vesting/addManyUser: accounts and amounts's length should equal");
+        require(
+            _tgeTimestamp > block.timestamp,
+            "Vesting/constructor: TGE timestamp must be greater than block timestamp"
+        );
+        require(
+            _unlockPeriod > 0,
+            "Vesting/constructor: Unlock Period must be greater than zero"
+        );
 
-        for (uint256 index = 0; index < accounts.length; index++) {
-            addOneUser(accounts[index], amounts[index]);
+        token = IERC20(_token);
+        tgeTimestamp = _tgeTimestamp;
+        unlockPeriod = _unlockPeriod;
+    }
+
+    modifier mustNotAfterTge() {
+        require(!afterTge(), "Vesting: TGE happened");
+        _;
+    }
+
+    function afterTge() public view returns (bool) {
+        if (block.timestamp < tgeTimestamp) return false;
+        else return true;
+    }
+
+    function setTGETimestamp(uint256 _tgeTimestamp)
+        public
+        onlyOwner
+        mustNotAfterTge
+    {
+        require(
+            _tgeTimestamp > block.timestamp,
+            "Vesting/constructor: TGE timestamp must be greater than block timestamp"
+        );
+
+        tgeTimestamp = _tgeTimestamp;
+    }
+
+    function addUser(
+        address _account,
+        uint256 _amount,
+        uint256 _tgeUnlockPercentage,
+        uint256 _cliffPeriod,
+        uint256 _vestingDuration,
+        VestingType _vestingType
+    ) public onlyOwner {
+        _addUser(
+            _account,
+            _amount,
+            _tgeUnlockPercentage,
+            _cliffPeriod,
+            _vestingDuration,
+            _vestingType
+        );
+    }
+
+    function addManyUser(
+        address[] memory _accounts,
+        uint256[] memory _amounts,
+        uint256[] memory _tgeUnlockPercentages,
+        uint256[] memory _cliffPeriods,
+        uint256[] memory _vestingDurations,
+        VestingType[] memory _vestingTypes
+    ) public onlyOwner {
+        require(
+            _accounts.length > 0 &&
+                _accounts.length == _amounts.length &&
+                _amounts.length == _tgeUnlockPercentages.length &&
+                _tgeUnlockPercentages.length == _cliffPeriods.length &&
+                _cliffPeriods.length == _vestingDurations.length &&
+                _vestingDurations.length == _vestingTypes.length,
+            "Vesting: Invalid parameters"
+        );
+
+        for (uint256 i = 0; i < _accounts.length; i++) {
+            _addUser(
+                _accounts[i],
+                _amounts[i],
+                _tgeUnlockPercentages[i],
+                _cliffPeriods[i],
+                _vestingDurations[i],
+                _vestingTypes[i]
+            );
         }
     }
 
-    function removeUser(address account) public onlyOwner isUserInVesting(account) {
-        require(!_isSetSchedule || _schedule.startDate >= uint32(block.timestamp / SECONDS_PER_DAY), "Vesting/removeUser: can't remove user when vesting started");
-    
-        _totalAmountInvested -= _users[account].amount;
+    function _addUser(
+        address _account,
+        uint256 _amount,
+        uint256 _tgeUnlockPercentage,
+        uint256 _cliffPeriod,
+        uint256 _vestingDuration,
+        VestingType _vestingType
+    ) internal {
+        require(
+            _account != address(0),
+            "Vesting: User must be not zero address"
+        );
+        require(
+            userToVesting[_account].exist == false,
+            "Vesting: User already exists"
+        );
+        require(
+            _tgeUnlockPercentage <= 100,
+            "Vesting: TGE unlock percentage must be less than 100"
+        );
 
-        delete _users[account];
+        userToVesting[_account].amount = _amount;
+        userToVesting[_account].tgeUnlockPercentage = _tgeUnlockPercentage;
+        userToVesting[_account].cliffPeriod = _cliffPeriod;
+        userToVesting[_account].vestingDuration = _vestingDuration;
+        userToVesting[_account].vestingType = _vestingType;
+        userToVesting[_account].exist = true;
 
-        emit RemoveUser(account);
+        totalVesting += _amount;
+
+        if (token.balanceOf(address(this)) < totalVesting) {
+            token.safeTransferFrom(
+                _msgSender(),
+                address(this),
+                totalVesting - token.balanceOf(address(this))
+            );
+        }
+
+        emit AddUser(_account, _amount);
     }
 
-    function withdraw() external isUserInVesting(msg.sender) isVestingScheduled(){
-        uint32 releaseTime = uint32(block.timestamp / SECONDS_PER_DAY);
-        User memory user = _users[msg.sender];
-        uint256 possibleWithdrawAmount;
+    function removeUser(address _account) public onlyOwner {
+        require(
+            userToVesting[_account].exist == true,
+            "Vesting: User does not exist"
+        );
 
-        if(_tgeUnlockPercent > 0) {
-            require(releaseTime >= _tgeUnlockDate, "Vesting/withdraw: can not withdraw TGE token before TGE unlock date");
-            if(user.tgeAmountClaimed == 0) {
-                uint256 tgeAmountClaim = (user.amount / _tgeUnlockPercent) / 100;
-                possibleWithdrawAmount += tgeAmountClaim;
-                user.tgeAmountClaimed += tgeAmountClaim;
-                emit WithdrawTGEUnlock(msg.sender, tgeAmountClaim);
-            }
-        } else {
-            require(releaseTime >= _schedule.cliffPeriodDate, "Vesting/withdraw: cliffPeriod not expired");
-        }
-    
-        if (releaseTime >= _schedule.startDate + _schedule.interval * _schedule.milestones) {
-            possibleWithdrawAmount += (user.amount - (user.amountClaimed + user.tgeAmountClaimed));
-        } else if (releaseTime >= _schedule.cliffPeriodDate) {
+        token.safeTransfer(
+            owner(),
+            userToVesting[_account].amount -
+                userToVesting[_account].amountClaimed
+        );
 
-            uint32 milestonesVested = (releaseTime - _schedule.cliffPeriodDate) / _schedule.interval + 1;
-            if(milestonesVested > _schedule.milestones) {
-                milestonesVested = _schedule.milestones;
-            }
-            uint256 vested = ((user.amount- user.tgeAmountClaimed) * milestonesVested) / _schedule.milestones;
-            possibleWithdrawAmount = vested - user.amountClaimed;
-        }
+        delete userToVesting[_account];
 
-        require(possibleWithdrawAmount > 0, "Vesting/withdraw: user withdraw zero token");
-
-        _users[msg.sender].amountClaimed += (possibleWithdrawAmount - user.tgeAmountClaimed);
-
-        _token.safeTransfer(msg.sender, possibleWithdrawAmount);
-
-        emit WithdrawToken(msg.sender, possibleWithdrawAmount);
+        emit RemoveUser(_account);
     }
 
+    function claim() public {
+        require(
+            userToVesting[_msgSender()].exist == true,
+            "Vesting: User does not exist"
+        );
+
+        uint256 claimableAmount = _getVestingClaimableAmount(_msgSender());
+
+        require(claimableAmount > 0, "Vesting: Nothing to claim");
+
+        userToVesting[_msgSender()].amountClaimed += claimableAmount;
+
+        token.safeTransfer(_msgSender(), claimableAmount);
+
+        emit Claim(_msgSender(), claimableAmount);
+    }
+
+    function getVestingClaimableAmount(address _user)
+        external
+        view
+        returns (uint256)
+    {
+        return _getVestingClaimableAmount(_user);
+    }
+
+    function _getVestingClaimableAmount(address _user)
+        internal
+        view
+        returns (uint256 claimableAmount)
+    {
+        UserVestingInfo memory info = userToVesting[_user];
+
+        if (block.timestamp < tgeTimestamp) return 0;
+
+        uint256 totalUnlock = (info.tgeUnlockPercentage * info.amount) / 100;
+
+        if (info.vestingType == VestingType.NEARLY) {
+            uint256 passedBlocks = (block.timestamp - tgeTimestamp) /
+                SECONDS_PER_BLOCK;
+
+            if (passedBlocks == 0) return totalUnlock;
+
+            totalUnlock =
+                totalUnlock +
+                ((((100 - info.tgeUnlockPercentage) * info.amount) / 100) *
+                    passedBlocks) /
+                (unlockPeriod * info.vestingDuration);
+
+            return totalUnlock - info.amountClaimed;
+        } else if (info.vestingType == VestingType.MONTHLY) {
+            uint256 passedBlocks = (block.timestamp - tgeTimestamp) /
+                SECONDS_PER_BLOCK;
+
+            if (passedBlocks == 0) return totalUnlock;
+
+            uint256 passedPeriods = passedBlocks / unlockPeriod;
+
+            totalUnlock =
+                totalUnlock +
+                ((((100 - info.tgeUnlockPercentage) * info.amount) / 100) *
+                    passedPeriods) /
+                info.vestingDuration;
+
+            return totalUnlock - info.amountClaimed;
+        }
+    }
 }
