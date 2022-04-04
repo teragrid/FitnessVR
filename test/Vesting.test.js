741092881253b2bdbab821e04c3d79e32348c2ee
kuz <k.kuz1997@gmail.com>
2022-04-04 15:50:01 +0700

    test

--- test/Vesting.test.js
+++ test/Vesting.test.js
@@ -1,40 +1,60 @@
-const timeMachine = require('ganache-time-traveler');
+// 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+// 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+// 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC
+// 0x90F79bf6EB2c4f870365E785982E1f101E93b906
+// 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65
+// 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc
+// 0x976EA74026E726554dB657fA54763abd0C3a0aa9
+// 0x14dC79964da2C08b23698B3D3cc7Ca32193d9955
+// 0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f
+// 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720
+// 0xBcd4042DE499D14e55001CcbB24a551F3b954096
+// 0x71bE63f3384f5fb98995898A86B02Fb2426c5788
+// 0xFABB0ac9d68B0B445fB7357272Ff202C5651694a
+// 0x1CBd3b2770909D4e10f157cABC84C7264073C9Ec
+// 0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097
+// 0xcd3B766CCDd6AE721141F452C550Ca635964ce71
+// 0x2546BcD3c84621e976D8185a91A922aE77ECEc30
+// 0xbDA5747bFD65F08deb54cb465eB87D40e51B197E
+// 0xdD2FD4581271e230360230F9337D5c0430Bf44C0
+// 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199
+const { advanceBlock, advanceBlockTo } = require("@openzeppelin/test-helpers/src/time");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-let MUUVToken = artifacts.require('MUUV');
-let Vesting = artifacts.require('Vesting');
+const  current = Math.floor(Date.now() / 1000);
 
 
-contract('Vesting with ganache time traveler', async (accounts) =>  {
-
-    let token;
+let token;
     let pool;
-    let snapshot;
-    let snapshotId;
     let today;
 
     const SECONDS_IN_DAY = 86400;
     const MILISECONDS_IN_DAY = 86400000;
- 
+    const ONE_MILLION = 1000000;
+
     describe("test logic: ", async() =>{
 
         beforeEach(async() => {
-            snapshot = await timeMachine.takeSnapshot();
-            snapshotId = snapshot['result'];
-            token = await MUUVToken.deployed("MultiWorld", "MUUV");
-            pool = await Vesting.new(token.address);
-            today = Math.floor(snapshot['id'] / MILISECONDS_IN_DAY) + 1;
+            let MUUVToken = await ethers.getContractFactory('MUUV');
+            let Vesting = await ethers.getContractFactory('Vesting');
+            token = await MUUVToken.deploy();
+            pool = await Vesting.deploy(token.address, current + SECONDS_IN_DAY * 10, SECONDS_IN_DAY * 2);
         });
-     
-        afterEach(async() => {
-            await timeMachine.revertToSnapshot(snapshotId);
+    
+        it('1.1: mint token', async () => {
+
+            let x = (await token.balanceOf("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266")).toString();
+            console.log("x: " + x);
+
+            expect(x).to.equal("900000000000000000000000000");
         });
 
-        it('2.1.1: mint token', async () => {
-            await token.transfer(pool.address, 200000);
-            let userAmount2 = await token.balanceOf(pool.address);
-            console.log("userAmount2: " + userAmount2);
+        it('1.2: transfer token to Vesting', async () => {
+            await token.transfer(pool.address, 900000000);
+            let x = await token.balanceOf(pool.address);
+            console.log("x: " + parseInt(x));
 
-            assert.equal(userAmount2, 200000, "wrong");
+            expect(x).to.equal(900000000);
         });
     });
-})
